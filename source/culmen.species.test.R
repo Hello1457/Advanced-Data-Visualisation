df32d5aa4dd0c7245798bb966cf53e0f7adff146
Hello1457 <117754167+Hello1457@users.noreply.github.com>
2022-12-09 03:48:21 +0000

    Update culmen.species.test.R

--- source/culmen.species.test.R
+++ source/culmen.species.test.R
@@ -9,11 +9,13 @@ penguins_clean<-cleaning(penguins_raw)
 # saving clean data}
 write.csv(penguins_clean,"data_clean/penguins_clean.csv")
 
-# testing if culmen length is affected by species 
+# testing if culmen length is affected by species - a one way anova was used
 model1<-aov(culmen_length_mm~species,penguins_clean)
 summary(model1)
 
-
+# This one-way anova had an F-value of 410 and a P value of <2e-16. Given the p-value is below 0.05, 
+# we can accept the hypothesis that being from different species has a significant
+# effect on the average culmen length of a penguin in at least some of the species, and that the means are significantly different.
 
 
 # plot graph
